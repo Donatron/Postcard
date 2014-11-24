3560a434da7ded876339058072bcda67a80cf4c6
Donatron <don.macarthur68@bigpond.com>
2014-11-24 11:21:57 +1000

    added nameLabel

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -29,7 +29,6 @@ class ViewController: UIViewController {
 
     @IBAction func sendMailButtonPressed(sender: UIButton)
     {
-        // adding a comment here to test commits
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
